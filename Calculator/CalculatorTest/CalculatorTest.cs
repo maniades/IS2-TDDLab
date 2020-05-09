f0b227cf6a5118cf08f03e7d6dde6d0a10c4fbfd
hectorjl <hectorjl@gmail.com>
2020-05-09 13:47:09 +0200

    typos and some comments

--- Calculator/CalculatorTest/CalculatorTest.cs
+++ Calculator/CalculatorTest/CalculatorTest.cs
@@ -3,16 +3,19 @@ using NUnit.Framework;
 
 namespace CalculatorTest
 {
+    [TestFixture]
     public class Tests
     {
         CalcEngine engine;
 
+        // This attribute allows this part of the test to be ran before each test takes place, initializing the class to test in this case
         [SetUp]
         public void Setup()
         {
             engine = new CalcEngine();
         }
 
+        // We are doing three happy patch tests here
         [Test]
         public void SumaTestOk1()
         {
@@ -24,7 +27,7 @@ namespace CalculatorTest
             // Act
             int result = engine.Sum(value1, value2);
 
-            // Asset
+            // Assert
             Assert.AreEqual(result, expectedResult);
         }
 
@@ -39,7 +42,7 @@ namespace CalculatorTest
             // Act
             int result = engine.Sum(value1, value2);
 
-            // Asset
+            // Assert
             Assert.AreEqual(result, expectedResult);
         }
 
@@ -54,8 +57,12 @@ namespace CalculatorTest
             // Act
             int result = engine.Sum(value1, value2);
 
-            // Asset
+            // Assert
             Assert.AreEqual(result, expectedResult);
         }
+
+
+        // There is not a lot more to test here, as a sum function does not do a lot, and does not have a lot of input requirements.
+        // Perhaps some out of bounds tests (though testing that would be really testing the compiler, not the program)
     }
 }
