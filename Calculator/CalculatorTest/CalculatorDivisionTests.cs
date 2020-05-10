1cdaab1d4750a0322e644976795fee990919c9cb
hectorjl <hectorjl@gmail.com>
2020-05-10 16:29:50 +0200

    hechos los tests para la división. Los tests son normales, de happy path, excepto uno extra que comprueba que al dividir entre cero efectivamente se lanza una división de división por cero

---
+++ Calculator/CalculatorTest/CalculatorDivisionTests.cs
@@ -0,0 +1,82 @@
+using Calculator;
+using NUnit.Framework;
+using System;
+
+namespace CalculatorTest
+{
+    [TestFixture]
+    public class CalculatorDivisionTests
+    {
+        CalcEngine engine;
+
+        // This attribute allows this part of the test to be ran before each test takes place, initializing the class to test in this case
+        [SetUp]
+        public void Setup()
+        {
+            engine = new CalcEngine();
+        }
+
+        // We are doing some happy patch tests here
+        [Test]
+        public void DivisionTestOk1()
+        {
+            // Arrange
+            int value1 = 4;
+            int value2 = 2;
+            int expectedResult = 2;
+
+            // Act
+            int result = 0;
+            //int result = engine.Division(value1, value2);
+
+            // Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        // We are doing some happy patch tests here
+        [Test]
+        public void DivisionTestOk2()
+        {
+            // Arrange
+            int value1 = 0;
+            int value2 = 5;
+            int expectedResult = 0;
+
+            // Act
+            int result = -10; // Juat to make to test fail
+            //int result = engine.Division(value1, value2);
+
+            // Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        // We are doing some happy patch tests here
+        [Test]
+        public void DivisionTestOk3()
+        {
+            // Arrange
+            int value1 = -6;
+            int value2 = 2;
+            int expectedResult = -3;
+
+            // Act
+            int result = 0;
+            //int result = engine.Division(value1, value2);
+
+            // Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        // For division, we are doing another test that will throw an exception
+        [Test]
+        public void DivisionException1()
+        {
+            // Arrange
+            int value1 = 10;
+            int value2 = 0;
+
+            // Act & assert
+            Assert.Throws<DivideByZeroException>(() => engine.Division());
+        }
+    }
+}
